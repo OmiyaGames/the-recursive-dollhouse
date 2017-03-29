d85deb8c2e2899f724434fdb25e8a76b7c038871
Taro Omiya <taro.omiya@omiyagames.com>
2017-03-29 18:40:53 -0400

    Fixing the interactie decorations!

--- Assets/Project/Scripts/InteractiveDecoration.cs
+++ Assets/Project/Scripts/InteractiveDecoration.cs
@@ -1,16 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class InteractiveDecoration : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody))]
+public class InteractiveDecoration : TierObject
+{
+    public const int MaxTierDistance = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    float startingForce = 0f;
+
+    Rigidbody body;
+    Vector3 originalLocalPosition;
+    Quaternion originalLocalRotation;
+
+    void Start()
+    {
+        ResizeParent.Instance.OnBeforeResize += OnBeforeResize;
+        ResizeParent.Instance.OnAfterResize += OnAfterResize;
+
+        body = GetComponent<Rigidbody>();
+        originalLocalPosition = transform.localPosition;
+        originalLocalRotation = transform.localRotation;
+    }
+
+    public override void SetTheme(MoodTheme theme)
+    {
+        // Do nothing
+    }
+
+    protected override void OnThisTierChanged(ResizingTier obj)
+    {
+        // Do nothing
+    }
+
+    private void OnAfterResize(ResizeParent obj)
+    {
+        // Enable body
+        body.isKinematic = false;
+    }
+
+    private void OnBeforeResize(ResizeParent obj)
+    {
+        // Disable body movement
+        body.isKinematic = true;
+
+        // Check if this object should now be out of view
+        if((obj.CurrentTier - ThisTier) > MaxTierDistance)
+        {
+            body.velocity = Vector3.zero;
+            transform.localPosition = originalLocalPosition;
+            transform.localRotation = originalLocalRotation;
+        }
+    }
 }
--- Assets/Project/Scripts/ResizeParent.cs
+++ Assets/Project/Scripts/ResizeParent.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityStandardAssets.Characters.FirstPerson;
 using OmiyaGames;
+
 [DisallowMultipleComponent]
 public class ResizeParent : MonoBehaviour
 {
